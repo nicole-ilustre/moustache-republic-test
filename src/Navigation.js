6fd225b8f724037f408bd32938c98ab3e1c3d58a
nicole-ilustre <nicoleilustre2292@gmail.com>
2021-06-10 23:07:36 +1200

    add attempt at javascript functionality

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,14 +1,22 @@
 
-import React from 'react'
+
+import React, {useState, useEffect} from 'react'
+import { selectedItems } from './Product'
 
 export default function Navigation () {
+  const [cartItems, setCartItems] = useState([])
+
+  useEffect(() => {
+    setCartItems(selectedItems)
+  }, [selectedItems])
 
     return (
         <div className ="nav">
           <div className ="dropdown">
             <button className ="dropbtn">My Cart</button>
             <div class="dropdown-content">
-               <div className="cart-item">
+              {/* {cartItems.map((item, i) => <div key={i} className="cart-item">{item}</div>)} */}
+               <div className="cart-item"> 
                  <img id="product-selected" src="classic-tee.jpg" alt="classic tee"/>
                  <div className="product-text">1 x $75.00</div>
                  <div className="product-text">Size: Small</div>
