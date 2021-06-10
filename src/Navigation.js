444858a6580d1edd08e19128112a700a6d8cc9aa
nicole-ilustre <nicoleilustre2292@gmail.com>
2021-06-10 22:04:18 +1200

    add css to navigation.js

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,7 +1,20 @@
+
 import React from 'react'
 
 export default function Navigation () {
+
     return (
-        <div>Nav</div>
+        <div className ="nav">
+          <div className ="dropdown">
+            <button className ="dropbtn">My Cart</button>
+            <div class="dropdown-content">
+               <div className="cart-item">
+                 <img id="product-selected" src="classic-tee.jpg" alt="classic tee"/>
+                 <div className="product-text">1 x $75.00</div>
+                 <div className="product-text">Size: Small</div>
+              </div>
+            </div>
+          </div>
+        </div>
     )
 }
